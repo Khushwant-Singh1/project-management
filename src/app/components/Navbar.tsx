742eb29ed925452308ae0947cd0fc6ce81475043
Khushwant-Singh1 <khushwantzx@gmail.com>
2025-04-11 12:13:58 +0530

    feat: enhance Navbar with sidebar toggle functionality and refactor DashboardWrapper layout

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,12 +1,25 @@
 import React from "react";
-import { Search, Settings } from "lucide-react";
+import { Menu, Search, Settings } from "lucide-react";
 import Link from "next/link";
+import { useAppDispatch, useAppSelector } from "./redux";
+import { setIsSidebarCollapsed } from "@/state";
 
 const Navbar = () => {
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
   return (
     <div className="flex items-center justify-between bg-white px-4 py-3 dark:px-4 dark:py-3">
       Navbar
       <div className="flex items-center gap-8">
+        {!isSidebarCollapsed ? null : (
+          <button
+            onClick={() => dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))}
+          >
+            <Menu className="h-8 w-8 cursor-pointer" />
+          </button>
+        )}
         <div className="relative flex h-min w-[200px]">
           <Search className="absolute left-[4px] top-1/2  mr-2 h-5 w-5 -translate-y-1/2 transform cursor-poiter dark:text-white" />
           <input
@@ -18,7 +31,7 @@ const Navbar = () => {
       </div>
       <div className="flex items-center">
         <Link
-          href="settings"
+          href="/settings"
           className="h-min w-min rounded p-2  hover:bg-gray-100"
         >
           <Settings className="h-6 w-6 cursor-pointer" />
--- src/app/dashboardWrapper.tsx
+++ src/app/dashboardWrapper.tsx
@@ -1,17 +1,31 @@
+"use client";
 import React from "react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
-const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+import StoreProvider, { useAppSelector } from "./components/redux";
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+// Adjust the path to your store file
+  
+  const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed);
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
       <Sidebar />
       <main
-        className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}
+        className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg ${
+          isSidebarCollapsed ? "" : "md:pl-64"}`}
       >
         <Navbar />
         {children}
       </main>
     </div>
+  );
+};
+
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
   );
 };
 
